baac3ee13697f265f37c003e0197149a16a659a5
Arthur <arthurgren4@gmail.com>
2025-09-08 21:45:57 -0300

    codigo com a função de somar

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static int somar(int num1, int num2){
+        return num1 + num2;
+    }
+
     public static void main(String[] args) {
 
         Pessoa pessoa1 = new Pessoa();
@@ -8,5 +13,9 @@ public class Main {
         pessoa1.idade = 18;
         System.out.println(pessoa1.nome);
         System.out.println(pessoa1.idade);
+
+        int result = somar(5, 6);
+        System.out.println(result);
+        }
     }
 }
