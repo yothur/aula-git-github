397dd568aaf9eacf1581d7e261eb72ad01caa03f
Arthur <arthurgren4@gmail.com>
2025-09-08 21:06:58 -0300

    AlteraçÃaão de numeros da soma
    numeros da soma alterados

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     }
     public static void main(String[] args) {
 
-        int result = somar(5, 6);
+        int result = somar(3, 25);
         System.out.println(result);
     }
 }
